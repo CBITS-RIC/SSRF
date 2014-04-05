2eee0f2de9af51e0b68155c7d68bf414512de895
Luke3D <ingluca@gmail.com>
2014-04-04 19:11:28 -0500

    UCI

--- SSRF_nocell_multiClass.m
+++ SSRF_nocell_multiClass.m
@@ -14,8 +14,8 @@ rng('default')   %fix random number generator seed
 ntrees = 50;     %forest size
 T0 = 5;          %initial temperature
 alpha = 1;     %coeff to control the weight of the unlabeled part in the loss function
-epochs = 100;     %epochs for unlabeled training
-tau = 80;  %cooling fcn time constant
+epochs = 200;     %epochs for unlabeled training
+tau = 50;  %cooling fcn time constant
 
 
 
@@ -58,17 +58,38 @@ Ytrain = load('../Data/UCIHARDataset/train/y_train.txt');
 % Xtest = load('../Data/UCIHARDataset/test/X_test.txt');   
 % Ytest = load('../Data/UCIHARDataset/test/y_test.txt');   
 
+subjects = load('../Data/UCIHARDataset/train/subject_train.txt');
+subject_codes = unique(subjects);
+n_train = 1;
+n_test = 1;
+
+%%this should be made such that the training and test subjects are picked
+%randomly
+subj_train = subject_codes(1:n_train);
+subj_test  = subject_codes(n_train+1:n_train+n_test);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+ind_train = [];
+for i=1:n_train,
+    ind_train = [ind_train; find(subjects==subj_train(i))];
+end
+ind_test = [];
+for i=1:n_test,
+    ind_test = [ind_test; find(subjects==subj_test(i))];
+end
+
 n_class = length(unique(Ytrain)); %the # of classes
 classes = unique(Ytrain);         %the class codes
 
 %Randomly selects a portion of training data as labeled. Rest is Unlabeled
-labeled = zeros(size(Xtrain,1),1);
-labeled(randperm(size(Xtrain,1),round(size(Xtrain,1)*frac))) = 1;
-labeled = logical(labeled);
-Xl = Xtrain(labeled,:);   Yl = Ytrain(labeled);          %labeled data
-Xu = Xtrain(~labeled,:);  Yu = Ytrain(~labeled);   %unlabeled data
+% labeled = zeros(size(Xtrain,1),1);
+% labeled(randperm(size(Xtrain,1),round(size(Xtrain,1)*frac))) = 1;
+% labeled = logical(labeled);
+% Xl = Xtrain(labeled,:);   Yl = Ytrain(labeled);          %labeled data
+% Xu = Xtrain(~labeled,:);  Yu = Ytrain(~labeled);   %unlabeled data
+Xl = Xtrain(ind_train,:);   Yl = Ytrain(ind_train);          %labeled data
+Xu = Xtrain(ind_test,:);  Yu = Ytrain(ind_test);   %unlabeled data
 Yu_forest = zeros(ntrees*size(Xu,1),1);          %unlabeled data for entire forest
-
 
 
 %% Train RF on labeled data
@@ -89,25 +110,25 @@ acc(1) = sum(Yu==Yfu)/length(Yu)
 %% DA optimization
 
 lgs=[];
-for m = 1:epochs
+for m = 1:epochs,
     T = T0*exp(-(m-1)/tau);     %reduce temp value
     Tvals(m) = T;               %save T values
     
     [Yu_p,Pu] = predict(forest,Xu);    %compute prob Pu_i of each class (i) for the unlabeled data (output prob of forest)
-
+    
     %     Yu_p = str2num(cell2mat(Yu_p));
-%     [r0,c0] = find(Pu == 0);      %find zero P values (to prevent log(0)=-inf)
-%     Pu(r0,c0) = eps;
-%     Pu = Pu - .5;
-
-%margin max loss fcn (Entropy)
-for c = 1:n_class
-    lg(:,c) = exp(-2*(Pu(:,c)-max(Pu(:,classes(classes ~= c)),[],2)));  
-    lgs = [lgs; mean(mean(lg))];
-end
+    %     [r0,c0] = find(Pu == 0);      %find zero P values (to prevent log(0)=-inf)
+    %     Pu(r0,c0) = eps;
+    %     Pu = Pu - .5;
+    
+    %margin max loss fcn (Entropy)
+    for c = 1:n_class
+        lg(:,c) = exp(-2*(Pu(:,c)-max(Pu(:,classes(classes ~= c)),[],2)));
+        lgs = [lgs; mean(mean(lg))];
+    end
     
     %Compute Optimal Distribution over predicted labels
-    Pu_opt = exp(-(alpha*lg+T)/T); 
+    Pu_opt = exp(-(alpha*lg+T)/T);
     Pu_opt = max(Pu_opt,eps);       %prevents Nan if Pu_opt is 0
     Z = sum(Pu_opt,2); Z = repmat(Z,[1 n_class]);
     Pu_opt = Pu_opt./Z; %normalized probabilities
