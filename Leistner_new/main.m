feb2656bcfdefa664cbb9a07ca857ee43aecb539
sosata <rob.taheri@gmail.com>
2014-04-22 15:13:57 -0500

    experiments

--- Leistner_new/main.m
+++ Leistner_new/main.m
@@ -2,8 +2,8 @@ clear all;
 close all;
 
 subj = 3;   %for few-samples and cross-trial (subject no.)
-subj_lab = 1; %for cross-subject (labeled)
-subj_unlab = 2:10;   %for cross-subject (unlabeled)
+subj_lab = 2; %for cross-subject (labeled)
+subj_unlab = 1;   %for cross-subject (unlabeled)
 Nsamp = 2;  %for few-samples (no. samples)
 RepFac = 1; %for few-samples (1 = equal; 0 = no balance)
 trial = 1;  %for cross-trial (trial no. for labeled)
@@ -31,12 +31,4 @@ for k = 1:10,
     acc_ = [acc_; acc];
 end
 
-figure;
-acc_init = acc_(:,1);
-acc_good = acc_(:,end); acc_good(acc_good<acc_init) = 0;
-acc_bad = acc_(:,end); acc_bad(acc_bad>=acc_init) = 0;
-bar([acc_init, acc_good]);
-colormap([0 0 1; 0 1 0]);
-figure;
-bar([acc_init, acc_bad]);
-colormap([0 0 1; 1 0 0]);
+bar([acc_(:,1), acc_(:,end)]);
