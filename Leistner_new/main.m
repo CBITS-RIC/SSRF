ae0e278a28e174e6268b7a15f04b26e5527b749d
Luke3D <ingluca@gmail.com>
2014-04-24 15:36:42 -0500

    added save png to main.m

--- Leistner_new/main.m
+++ Leistner_new/main.m
@@ -6,14 +6,14 @@ simtype = simtypes{1};
 
 %for few-samples and cross-trial (subject no.)
 %each row is for one run, columnns are subjects in a multi-subject run
-subjects = 1:30;
+subjects = [1:30]';
 subj_lab = 1; %for cross-subject (labeled)
 subj_unlab = 2:30;   %for cross-subject (unlabeled)
 nsamp = 1;  %for few-samples (no. samples per trial)
 RepFac = 1; %for few-samples (1 = equal; 0 = no balance)
 trial = 1;  %for cross-trial (trial no. for labeled)
 
-ntrees = 50;
+ntrees = 10;
 epochs = 20;
 T0 = 1;
 tau = 40;
@@ -51,9 +51,10 @@ for i = 1:size(subjects,1),
     end
 end
 
+%accuracy over runs and subjects
 figure;
 for i=1:size(subjects,1),
-%     subplot(size(subjects,1)/5, 5, i);
+    subplot(size(subjects,1)/5, 5, i);
     bar([acc_{i}(:,1), acc_{i}(:,end)]);
     imp(:,i) = (acc_{i}(:,end) - acc_{i}(:,1))./acc_{i}(:,1);
     ylim([.5 1]);
@@ -62,10 +63,11 @@ for i=1:size(subjects,1),
 
 end
 savefig(sprintf('%s_accs_ntrees%d_nsamp%d_nsub%d', simtype, ntrees, nsamp, size(subjects,2)));
+saveas(gcf,sprintf('%s_accs_ntrees%d_nsamp%d_nsub%d', simtype, ntrees, nsamp, size(subjects,2)),'png')
 
 figure;
 for i=1:size(subjects,1),
-%     subplot(size(subjects,1)/5, 5, i);
+    subplot(size(subjects,1)/5, 5, i);
     imp_good = imp(:,i);
     imp_bad = imp(:,i);
     imp_good(imp_good<0) = 0;
@@ -74,27 +76,33 @@ for i=1:size(subjects,1),
     hold on;
     bar(imp_bad, 'r');
     xlim([0 size(acc_{1},1)+1]);
-    set(gca,'FontSize',16)
+    set(gca,'FontSize',14)
+    ylim([-0.2 0.2])
 end
 savefig(sprintf('%s_imp_ntrees%d_nsamp%d_nsub%d', simtype, ntrees, nsamp, size(subjects,2)));
+saveas(gcf,sprintf('%s_imp_ntrees%d_nsamp%d_nsub%d', simtype, ntrees, nsamp, size(subjects,2)),'png');
+
 
 %improvement over different experiments
 figure;
-boxplot(imp);   set(gca,'FontSize',16)
+boxplot(imp);   set(gca,'FontSize',14)
 savefig(sprintf('%s_imp_boxplot_ntrees%d_nsamp%d_nsub%d', simtype, ntrees, nsamp, size(subjects,2)));
+saveas(gcf,sprintf('%s_imp_boxplot_ntrees%d_nsamp%d_nsub%d', simtype, ntrees, nsamp, size(subjects,2)),'png');
 
 %overall improvement
 figure;
-boxplot(imp(:));     set(gca,'FontSize',16)
+boxplot(imp(:));     set(gca,'FontSize',14)
 savefig(sprintf('%s_imp_boxplot_all_ntrees%d_nsamp%d_nsub%d', simtype, ntrees, nsamp, size(subjects,2)));
+saveas(gcf,sprintf('%s_imp_boxplot_all_ntrees%d_nsamp%d_nsub%d', simtype, ntrees, nsamp, size(subjects,2)),'png');
 
 %display accuracy at start and end for each subject
 figure;
 for i=1:size(subjects,1),
-%     subplot(size(subjects,1)/5, 5, i);
-%     boxplot(acc_{i}(:,end));     %accuracy at end
+    subplot(size(subjects,1)/5, 5, i);
     boxplot([acc_{i}(:,1) acc_{i}(:,end)]);   %accuracy at start and end 
     set(gca,'FontSize',16)
     set(gca, 'xtick', [1 2], 'xticklabel', {'initial','final'});
+    ylim([0.5 1])
 end
 savefig(sprintf('%s_acc_boxplot_all_ntrees%d_nsamp%d_nsub%d', simtype, ntrees, nsamp, size(subjects,2)));
+saveas(gcf,sprintf('%s_acc_boxplot_all_ntrees%d_nsamp%d_nsub%d', simtype, ntrees, nsamp, size(subjects,2)),'png');
